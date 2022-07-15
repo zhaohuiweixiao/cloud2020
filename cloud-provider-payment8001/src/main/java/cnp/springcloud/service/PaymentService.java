9e11dd51ea1bf42caccc40841a18d82924820fa6
zhaohuihui <zhaohuihui_yewu@cmss.chinamobile.com>
2022-07-15 18:10:06 +0800

    init project
    Signed-off-by: zhaohuihui <zhaohuihui_yewu@cmss.chinamobile.com>

---
+++ cloud-provider-payment8001/src/main/java/cnp/springcloud/service/PaymentService.java
@@ -0,0 +1,10 @@
+package cnp.springcloud.service;
+
+import cnp.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
